d933a44a9083e082e9c37e6ee7c12a4576aceba1
Xiaocong Lau <12184795@qq.com>
2019-04-28 21:37:41 +0800

    Init

---
+++ precache-manifest.68178d0492a2a0c0240f8da29bbf2b87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d09ae976aabbbdbe7d0",
+    "url": "/static/css/main.41382ad3.chunk.css"
+  },
+  {
+    "revision": "4d09ae976aabbbdbe7d0",
+    "url": "/static/js/main.5f51473a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8068b8f776049aeeeceb",
+    "url": "/static/js/2.9674d972.chunk.js"
+  },
+  {
+    "revision": "77e8e593ac4546d64fb3851fd90e3478",
+    "url": "/index.html"
+  }
+];
